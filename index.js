186bee9b92cac342d5790b1d805067f453145419
teakyungg <skeadee@users.noreply.github.com>
2025-02-02 16:08:54 +0900

    1.0.3

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import './TextDisplay.css';
+
+// 모듈 만들때 여기서 style를 변경하는 것은 최종형태에 들어감
+// 즉 여기서 변경한 값은 다른 프로젝트의 css에서 일반적인 형태로 변경이 불가능함 
+
 function TextDisplay({
-  className,
   title,
-  contents
+  contents,
+  basicColor
 }) {
   let Title = [];
   let Contents = [];
   Title.push(DataArraySet(title).map(item => /*#__PURE__*/_jsx("h2", {
-    style: {
-      margin: '0'
-    },
     children: item
   })));
   Contents.push(DataArraySet(contents).map(item => /*#__PURE__*/_jsx("p", {
-    style: {
-      margin: '0'
-    },
     children: item
   })));
   return /*#__PURE__*/_jsxs("div", {
-    className: className,
+    className: "text_display",
     style: {
-      padding: '20px'
+      backgroundColor: `${basicColor}`
     },
     children: [/*#__PURE__*/_jsx("div", {
       className: "display_title",
       children: Title
     }), /*#__PURE__*/_jsx("div", {
       className: "display_contents",
-      style: {
-        marginTop: '20px'
-      },
       children: Contents
     })]
   });
--- original.js
+++ original.js
@@ -1,20 +1,24 @@
-function TextDisplay({ className, title, contents }) {
+import './TextDisplay.css';
+
+// 모듈 만들때 여기서 style를 변경하는 것은 최종형태에 들어감
+// 즉 여기서 변경한 값은 다른 프로젝트의 css에서 일반적인 형태로 변경이 불가능함 
+
+function TextDisplay({ title, contents, basicColor}) {
 
     let Title = [];
     let Contents = [];
 
-    Title.push(DataArraySet(title).map((item) => <h2 style={{ margin: '0' }}>{item}</h2>));
-    Contents.push(DataArraySet(contents).map((item) => <p style={{ margin: '0' }}>{item}</p>))
-
+    Title.push(DataArraySet(title).map((item) => <h2>{item}</h2>));
+    Contents.push(DataArraySet(contents).map((item) => <p>{item}</p>))
 
     return (
-        <div className={className} style={{ padding: '20px' }}>
+        <div className="text_display" style={{backgroundColor:`${basicColor}`}}>
 
             <div className="display_title">
                 {Title}
             </div>
 
-            <div className="display_contents" style={{ marginTop: '20px' }}>
+            <div className="display_contents">
                 {Contents}
             </div>
 
